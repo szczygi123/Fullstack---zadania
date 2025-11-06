6efafc01ae9d0cef28319bab1b5681f1ee20d26e
szczygi123 <michal.szczygiel@onet.com.pl>
2025-11-06 14:08:28 +0100

    zrobione 1.3 - 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -22,9 +22,7 @@ const Content = (props) => {
 
 const Total = (props) => {
   const total =
-    props.course.parts[0].exercises +
-    props.course.parts[1].exercises +
-    props.course.parts[2].exercises
+    props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
 
   return (
     <p>Number of exercises {total}</p>
