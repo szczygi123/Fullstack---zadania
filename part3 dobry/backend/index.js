9771ef827e4bd13c6f83599e1e55deb314b04e77
Michał Szczygieł <michal.szczygiel@onet.com.pl>
2026-05-21 10:17:47 +0200

    zrobione part3 c

--- part3 dobry/backend/index.js
+++ part3 dobry/backend/index.js
@@ -12,15 +12,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
-
-  next(error)
-}
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
@@ -35,11 +26,12 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
     .catch(error => next(error))
-  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -54,7 +46,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -67,10 +59,10 @@ app.post('/api/persons', (request, response) => {
   })
 
   person.save()
-  .then(savedPerson => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -85,9 +77,10 @@ app.put('/api/persons/:id', (request, response, next) => {
       person.name = name
       person.number = number
 
-      return person.save().then((updatedPerson) => {
-        response.json(updatedPerson)
-      })
+      return person.save()
+        .then((updatedPerson) => {
+          response.json(updatedPerson)
+        })
     })
     .catch(error => next(error))
 })
@@ -117,8 +110,17 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
-app.use(errorHandler)
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
@@ -126,142 +128,3 @@ app.listen(PORT, () => {
 })
 
 
-// require('dotenv').config()
-// const express = require('express')
-// const morgan = require('morgan')
-// const Person = require('./models/person')
-// const app = express()
-
-// // Middleware logujące (własne)
-// const requestLogger = (request, response, next) => {
-//   console.log('Method:', request.method)
-//   console.log('Path:  ', request.path)
-//   console.log('Body:  ', request.body)
-//   console.log('---')
-//   next()
-// }
-
-// // Główne middleware parsujące i logujące
-// app.use(express.static('dist'))
-// app.use(express.json())
-// app.use(requestLogger)
-
-// // Konfiguracja Morgana do logowania zawartości body (dla POST/PUT)
-// morgan.token('body', (req, res) => JSON.stringify(req.body))
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-// // --- TRASY (ENDPOINTS) ---
-
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>')
-// })
-
-// // Zadanie 3.18: Pobieranie informacji ogólnych z bazy danych
-// app.get('/info', (request, response, next) => {
-//   Person.countDocuments({})
-//     .then(count => {
-//       response.send(`
-//         <p>Phonebook has info for ${count} people</p>
-//         <p>${new Date()}</p>
-//       `)
-//     })
-//     .catch(error => next(error))
-// })
-
-// // Pobieranie wszystkich osób
-// app.get('/api/persons', (request, response, next) => {
-//   Person.find({})
-//     .then(persons => {
-//       response.json(persons)
-//     })
-//     .catch(error => next(error))
-// })
-
-// // Zadanie 3.18: Pobieranie pojedynczego wpisu z bazy danych
-// app.get('/api/persons/:id', (request, response, next) => {
-//   Person.findById(request.params.id)
-//     .then(person => {
-//       if (person) {
-//         response.json(person)
-//       } else {
-//         response.status(404).end()
-//       }
-//     })
-//     .catch(error => next(error))
-// })
-
-// // Tworzenie nowego wpisu
-// app.post('/api/persons', (request, response, next) => {
-//   const body = request.body
-
-//   if (!body.name || !body.number) {
-//     return response.status(400).json({ error: 'name or number missing' })
-//   }
-  
-//   const person = new Person({
-//     name: body.name,
-//     number: body.number,
-//   })
-
-//   person.save()
-//     .then(savedPerson => {
-//       response.json(savedPerson)
-//     })
-//     .catch(error => next(error))
-// })
-
-// // Zadanie 3.17*: Aktualizacja istniejącego wpisu (PUT)
-// app.put('/api/persons/:id', (request, response, next) => {
-//   const { name, number } = request.body
-
-//   Person.findById(request.params.id)
-//     .then(person => {
-//       if (!person) {
-//         return response.status(404).end()
-//       }
-
-//       // Aktualizacja pól obiektu wyciągniętego z bazy
-//       person.name = name
-//       person.number = number
-
-//       return person.save().then((updatedPerson) => {
-//         response.json(updatedPerson)
-//       })
-//     })
-//     .catch(error => next(error))
-// })
-
-// // Zadanie 3.15: Usuwanie wpisu z bazy danych
-// app.delete('/api/persons/:id', (request, response, next) => {
-//   Person.findByIdAndDelete(request.params.id)
-//     .then(result => {
-//       response.status(204).end()
-//     })
-//     .catch(error => next(error))
-// })
-
-// // --- MIDDLEWARE OBSŁUGI KOŃCOWEJ ---
-
-// // Obsługa nieznanych punktów końcowych
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: 'unknown endpoint' })
-// }
-// app.use(unknownEndpoint)
-
-// // Zadanie 3.16: Centralny Middleware do obsługi błędów
-// const errorHandler = (error, request, response, next) => {
-//   console.error(error.message)
-
-//   if (error.name === 'CastError') {
-//     return response.status(400).send({ error: 'malformatted id' })
-//   } 
-
-//   next(error)
-// }
-// app.use(errorHandler)
-
-// // Uruchomienie serwera
-// const PORT = process.env.PORT || 3001
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
